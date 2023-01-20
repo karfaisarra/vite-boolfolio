3e9143990a5aaea99bc3fe1057c099001972d55d
karfaisarra <karfaisarra@gmail.com>
2023-01-20 14:54:02 +0100

    add views

---
+++ src/views/BlogView.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: 'BlogView'
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/SingleProjectView.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: 'SingleProjectView'
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
