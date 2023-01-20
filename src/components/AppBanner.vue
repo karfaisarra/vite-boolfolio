ad8c3271a96553d90bf445e21cb4d5da772e16c4
karfaisarra <karfaisarra@gmail.com>
2023-01-20 15:54:48 +0100

    add banner component

---
+++ src/components/AppBanner.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    name: 'AppBanner',
+    props: {
+        title: String
+    }
+}
+</script>
+
+<template>
+    <div class="banner shadow-sm">
+        <div class="container">
+            <h1 class="py-4 text-uppercase">{{ title }}</h1>
+        </div>
+    </div>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
