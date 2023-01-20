eada676fd7f1c405814478e34a379cce4c80dfb8
karfaisarra <karfaisarra@gmail.com>
2023-01-20 14:53:33 +0100

    add routes and update main.js

---
+++ src/router.js
@@ -0,0 +1,37 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from './views/HomeView.vue'
+import BlogView from './views/BlogView.vue'
+import AboutView from './views/AboutView.vue'
+import ContactsView from './views/ContactsView.vue'
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomeView
+        },
+        {
+            path: '/blog',
+            name: 'blog',
+            component: BlogView
+        },
+        {
+            path: '/blog/:slug',
+            name: 'single-project',
+            component: SingleProjectView
+        },
+        {
+            path: '/about',
+            name: 'about',
+            component: AboutView
+        },
+        {
+            path: '/contacts',
+            name: 'contacts',
+            component: ContactsView
+        },
+    ]
+});
+export { router };
